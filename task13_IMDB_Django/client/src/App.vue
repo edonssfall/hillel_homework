74861b029d07d2999cad29c1f570e67cd335b887
Leonid Tsarov <edonssfall123@gmail.com>
2022-09-17 15:09:57 +0200

    v0.00

---
+++ task13_IMDB_Django/client/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from "./components/common/Navbar.vue"
+import {onMounted} from "vue"
+import { useUserStore} from "./stores/user";
+
+const userStore = useUserStore()
+onMounted(
+    async () => {
+      await userStore.fetchUser()
+    }
+)
+</script>
+
+<template>
+  <header>
+    <Navbar />
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ task13_IMDB_Django/client/src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<script setup>
+import LoginForm from "../components/LoginForm.vue";
+import {useUserStore} from "../stores/user";
+import {onBeforeMount} from "vue";
+
+const userStore = useUserStore();
+
+onBeforeMount( () =>
+{
+  if (userStore.getUserId) {
+    location.href = '/';
+  }
+})
+
+</script>
+
+<template>
+  <main>
+    <LoginForm />
+  </main>
+</template>
